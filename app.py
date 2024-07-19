d5271a41e4938d9313fd7b58b20df0448c81ee5a
Bruuuh24 <nicholassok2@gmail.com>
2024-07-19 19:05:50 +1200

    Added - product.html

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 from flask import Flask, render_template
+import mypysql
+import hashlib 
+
 
 app = Flask(__name__)
 
+def encrypt(password):
+    return hashlib.sha256(password.encode()).hexdigest()
+
 @app.route("/")
 def home_page():
-    render_template("home.html")
+    return render_template("home.html")
+
+@app.route("/about")
+def about_page():
+    return render_template("about.html")
+
+@app.route("/login")
+def login_page():
+    return render_template("login.html")
 
 app.run(debug = True)
