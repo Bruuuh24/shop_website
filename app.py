f9d324c5ec724bb0b5fcff3a690084b9759b1abd
Bruuuh24 <nicholassok2@gmail.com>
2024-07-24 12:17:49 +1200

    Added - review.html - signup.html

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template, request, redirect, abort, flash, session
 import pymysql
 import hashlib 
+import json
+
+def load_products():
+    global products
+    with open("products.txt", "r") as file:
+        products = json.load(file)
+
+def save_products():
+    with open("products.txt", "w") as file:
+        json.dump(products, file)
 
 def create_connection():
     return pymysql.connect(
@@ -31,4 +41,19 @@ def login_page():
     #        cursor.excute
     return render_template("login.html")
 
+@app.route("/signup", methods = ["GET", "POST"])
+def signup_page():
+    if request.method == "GET":
+        return render_template("signup.html")
+    elif request.method == "POST":
+        with create_connection() as connection:
+            with connection.cursor() as cursor:
+                username = request.form["username"]
+                password_org = request.form["password"]
+                password_final = encrypt(password_org)
+                sql = "INSERT into users (username, password) VALUES(%s, %s)"
+                cursor.execute(sql, username, password_final)
+                connection.commit()
+            return render_template("signup.html")
+    
 app.run(debug = True)
