8f8a0aaab76d209fd3971f7efbc5c5c89122075f
Bruuuh24 <nicholassok2@gmail.com>
2024-08-28 20:14:25 +1200

    Added - foreign key to database

--- database.sql
+++ database.sql
@@ -11,7 +11,7 @@
  Target Server Version : 90001 (9.0.1)
  File Encoding         : 65001
 
- Date: 27/08/2024 21:51:04
+ Date: 28/08/2024 20:14:05
 */
 
 SET NAMES utf8mb4;
@@ -46,7 +46,7 @@ CREATE TABLE `products`  (
   `desciption` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `benfits` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+) ENGINE = InnoDB AUTO_INCREMENT = 10 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 
 -- ----------------------------
 -- Records of products
@@ -83,14 +83,16 @@ CREATE TABLE `users`  (
   `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `account_type` int NOT NULL,
-  PRIMARY KEY (`id`, `username`, `password`, `account_type`) USING BTREE
+  PRIMARY KEY (`id`, `username`, `password`, `account_type`) USING BTREE,
+  INDEX `id`(`account_type` ASC) USING BTREE,
+  CONSTRAINT `id` FOREIGN KEY (`account_type`) REFERENCES `account_type` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE = InnoDB AUTO_INCREMENT = 17 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
 
 -- ----------------------------
 -- Records of users
 -- ----------------------------
 BEGIN;
-INSERT INTO `users` (`id`, `username`, `password`, `account_type`) VALUES (1, 'test', '50c0152c2952082aeaf427885a2f617d67cf6de183a8816c0955ea5b875a216b', 1), (2, 'test2', '50c0152c2952082aeaf427885a2f617d67cf6de183a8816c0955ea5b875a216b', 2), (13, 'test3', 'b819462a81cd04dbabfe87db6bb30678baaaa74cc078a6d219b13aeb4c579ece', 1), (14, 'test4', 'eefefd2b42ee468b46da4597e68fd6f69d89b0caeca05ca96b5014abef6b5cdd', 1), (15, 'test5', '2e0b8d61fa2a6959d254b6ff5d0fb512249329097336a35568089933b49abdde', 1), (16, 'test6 ', 'e81dfe69841ad2f7b5790b63e998f0febaf3b29acd732881975130761b98e2c7', 1);
+INSERT INTO `users` (`id`, `username`, `password`, `account_type`) VALUES (1, 'test', '50c0152c2952082aeaf427885a2f617d67cf6de183a8816c0955ea5b875a216b', 1), (13, 'test3', 'b819462a81cd04dbabfe87db6bb30678baaaa74cc078a6d219b13aeb4c579ece', 1), (14, 'test4', 'eefefd2b42ee468b46da4597e68fd6f69d89b0caeca05ca96b5014abef6b5cdd', 1), (15, 'test5', '2e0b8d61fa2a6959d254b6ff5d0fb512249329097336a35568089933b49abdde', 1), (16, 'test6 ', 'e81dfe69841ad2f7b5790b63e998f0febaf3b29acd732881975130761b98e2c7', 1), (2, 'test2', '50c0152c2952082aeaf427885a2f617d67cf6de183a8816c0955ea5b875a216b', 2);
 COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
